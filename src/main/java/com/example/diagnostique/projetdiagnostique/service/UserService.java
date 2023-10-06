b1e23f26605a3a83e51506647539abab9d8e2933
Chakir-Aicha <aicha.chakir.2022@gmail.com>
2023-10-06 17:33:09 +0100

    commit1

--- src/main/java/com/example/diagnostique/projetdiagnostique/service/UserService.java
+++ src/main/java/com/example/diagnostique/projetdiagnostique/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.diagnostique.projetdiagnostique.service;
 
+import com.example.diagnostique.projetdiagnostique.DTO.UserDTO;
+
 public interface UserService {
+    String addUser(UserDTO userDTO);
 }
